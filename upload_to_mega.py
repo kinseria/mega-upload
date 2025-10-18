6cd3a2b1019a8e3b07eb159adb75713e776ebfca
kinseria <79118085+kinseria@users.noreply.github.com>
2025-10-18 23:07:05 +0500

    Create upload_to_mega.py

---
+++ upload_to_mega.py
@@ -0,0 +1,29 @@
+from mega import Mega
+import os
+import requests
+
+MEGA_EMAIL = os.getenv('MEGA_EMAIL')
+MEGA_PASSWORD = os.getenv('MEGA_PASSWORD')
+REMOTE_FILE_URL = os.getenv('REMOTE_FILE_URL')
+LOCAL_FILENAME = "downloaded_file"
+
+def download_file(url, local_path):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(local_path, 'wb') as f:
+        f.write(response.content)
+
+def upload_file_to_mega():
+    print("Downloading file from:", REMOTE_FILE_URL)
+    download_file(REMOTE_FILE_URL, LOCAL_FILENAME)
+
+    print("Logging into MEGA...")
+    mega = Mega()
+    m = mega.login(MEGA_EMAIL, MEGA_PASSWORD)
+
+    print("Uploading to MEGA...")
+    file = m.upload(LOCAL_FILENAME)
+    print("Uploaded file:", file)
+
+if __name__ == "__main__":
+    upload_file_to_mega()
